9bcda1e7cf259ba69ccbac82a8e6c96e3ecf42ba
Nebiyu <Nebiyuhiyabu@gmail.com>
2023-05-07 15:03:42 -0700

    setting up web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# this script prepares the necessary configurations to deploy a static page
+sudo apt-get update
+sudo apt-get install nginx
+sudo ufw allow 'Nginx HTTP'
+
+if [ ! -d /data/ ]; then
+	sudo mkdir /data/
+fi
+if [ ! -d /data/web_static/ ]; then
+	sudo mkdir /data/web_static/
+fi
+if [ ! -d /data/web_static/releases ]; then
+	sudo mkdir /data/web_static/releases/
+fi
+if [ ! -d /data/web_static/shared/ ]; then
+	sudo mkdir /data/web_static/shared/
+fi
+if [ ! -d /data/web_static/releases/test/ ]; then
+	sudo mkdir /data/web_static/releases/test/
+fi
+str=\
+"
+<html>
+<head><title>simple response</title></head>
+<body>ARTIMUS Leading Tech</body>
+</html>
+"
+if [ ! -f /data/web_static/releases/test/index.html ]; then
+	echo "$str" > /data/web_static/releases/test/index.html
+fi
+if [ -L /data/web_static/current ]; then
+	rm /data/web_static/current
+	ln -s /data/web_static/releases/test/ /data/web_static/current
+else
+	ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
