690639fea4b8d9c85cc4eb8503b1def051a10617
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-19 22:42:56 +0000

    deploying the archive file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,7 +29,7 @@ def do_deploy(archive_path):
         run("rm /tmp/{}".format(filename))
         run("rm -f /data/web_static/current")
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
-        .format(filename[:-4]))
+            .format(filename[:-4]))
         return True
     except Exception:
         return False
