1441a441787a7200908881f68095331cf4cce079
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-19 21:53:21 +0000

    small fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,21 @@
 from datetime import datetime
 from fabric.api import local
 import os
-""" 
-	These are modules we're going to use to pack the AirBnB file 
 """
+  These are modules we're going to use to pack the AirBnB file
+"""
+
+
 def do_pack():
-	"""
-		this function performs the task of
-		archiving the file web_static
-	"""
-	timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-	path = "versions/web_static_{}.tgz".format(timestamp)
-	if not os.path.exists("versions"):
-		local ("mkdir versions")
-	if (local("tar -cvzf {} web_static".format(path))):
-		return path
-	else:
-		return None
+  """
+    This function performs the task of
+    archiving the file web_static
+  """
+  timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+  path = "versions/web_static_{}.tgz".format(timestamp)
+  if not os.path.exists("versions"):
+    local ("mkdir versions")
+  if (local("tar -cvzf {} web_static".format(path))):
+    return path
+  else:
+    return None
