b50476bef29fcc12df37d68fc8731a8179a9ee08
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-19 21:56:54 +0000

    small fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,15 +8,15 @@ import os
 
 
 def do_pack():
-  """
+    """
     This function performs the task of
     archiving the file web_static
-  """
-  timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-  path = "versions/web_static_{}.tgz".format(timestamp)
-  if not os.path.exists("versions"):
-    local ("mkdir versions")
-  if (local("tar -cvzf {} web_static".format(path))):
-    return path
-  else:
-    return None
+    """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(timestamp)
+    if not os.path.exists("versions"):
+      local("mkdir versions")
+    if (local("tar -cvzf {} web_static".format(path))):
+      return path
+    else:
+      return None
